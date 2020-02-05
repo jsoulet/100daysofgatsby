7edf3f0960f78543c403b908576070f710141426
Johan Soulet <johan.soulet@gmail.com>
2020-02-06 00:11:25 +0100

    :pencil: new article about formik

--- src/components/ContactForm/component.tsx
+++ src/components/ContactForm/component.tsx
@@ -6,12 +6,8 @@ import { ContactFormValues } from './index'
 import Input from './Input'
 import validate from './validate'
 
-interface onSubmitInterface {
-  (values: ContactFormValues): void
-}
-
 interface ContactFormProps {
-  onSubmit: onSubmitInterface
+  onSubmit: (values: ContactFormValues) => void
 }
 
 const initialValues: ContactFormValues = {
@@ -25,69 +21,71 @@ const ContactForm: FunctionComponent<ContactFormProps> = ({ onSubmit }) => {
   const formik = useFormik<ContactFormValues>({
     initialValues,
     onSubmit,
-    validate
+    validate,
   })
+
   return (
     <form onSubmit={formik.handleSubmit} className="w-full mt-10">
-        <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-                <Input
-                    id="firstname"
-                    value={formik.values.firstname}
-                    onChange={formik.handleChange}
-                    error={!!formik.submitCount && formik.errors.firstname}
-                    placeholder="Bruce"
-                    label="First name"
-                />
-            </div>
-            <div className="w-full md:w-1/2 px-3">
-                <Input
-                    id="lastname"
-                    value={formik.values.lastname}
-                    onChange={formik.handleChange}
-                    error={!!formik.submitCount && formik.errors.lastname}
-                    placeholder="Wayne"
-                    label="Last name"
-                />
-            </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <Input
+            id="firstname"
+            value={formik.values.firstname}
+            onChange={formik.handleChange}
+            error={formik.submitCount > 0 && formik.errors.firstname}
+            placeholder="Bruce"
+            label="First name"
+          />
         </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-                <Input
-                    id="email"
-                    type="email"
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    error={!!formik.submitCount && formik.errors.email}
-                    placeholder="bruce@wayneenterprise.corp"
-                    label="Email"
-                />
-            </div>
+        <div className="w-full md:w-1/2 px-3">
+          <Input
+            id="lastname"
+            value={formik.values.lastname}
+            onChange={formik.handleChange}
+            error={formik.submitCount > 0 && formik.errors.lastname}
+            placeholder="Wayne"
+            label="Last name"
+          />
         </div>
-        <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-                <Input
-                    id="message"
-                    type="textarea"
-                    value={formik.values.message}
-                    onChange={formik.handleChange}
-                    error={!!formik.submitCount && formik.errors.message}
-                    label="Message"
-                    className="no-resize h-48 resize-y"
-                />
-            </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <Input
+            id="email"
+            type="email"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.submitCount > 0 && formik.errors.email}
+            placeholder="bruce@wayneenterprise.corp"
+            label="Email"
+          />
         </div>
-        <div className="md:flex md:items-center">
-            <div className="md:w-1/3">
-                <input
-                    value="Send"
-                    className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-                    type="submit"
-                />
-            </div>
+      </div>
+      <div className="flex flex-wrap -mx-3 mb-6">
+        <div className="w-full px-3">
+          <Input
+            id="message"
+            type="textarea"
+            value={formik.values.message}
+            onChange={formik.handleChange}
+            error={formik.submitCount > 0 && formik.errors.message}
+            label="Message"
+            className="no-resize h-48 resize-y"
+          />
+        </div>
+      </div>
+      <div className="md:flex md:items-center">
+        <div className="md:w-1/3">
+          <button
+            value="Send"
+            className="shadow bg-teal-400 hover:bg-teal-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >Submit</button>
+        </div>
         <div className="md:w-2/3"></div>
-        </div>
-    </form>)
+      </div>
+    </form>
+  )
 }
 
 export default ContactForm
